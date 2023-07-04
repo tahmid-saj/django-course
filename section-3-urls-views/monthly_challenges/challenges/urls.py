62617a4a792f793c71db7d47383ea8dd0a29c21f
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-03 20:21:04 -0400

    Redirect using reverse

--- section-3-urls-views/monthly_challenges/challenges/urls.py
+++ section-3-urls-views/monthly_challenges/challenges/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
   # path("february", views.feburary),
   # path("march", views.march),
   path("<int:month>", views.monthly_challenge_by_number),
-  path("<str:month>", views.monthly_challenge)
+  path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
