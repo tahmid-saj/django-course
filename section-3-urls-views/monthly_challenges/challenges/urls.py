d72458baaa360f5a94c0861027d1bd3cf1b93400
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-03 20:37:28 -0400

    Adding url links

--- section-3-urls-views/monthly_challenges/challenges/urls.py
+++ section-3-urls-views/monthly_challenges/challenges/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   # path("january", views.january),
   # path("february", views.feburary),
   # path("march", views.march),
+  path("", views.index),
   path("<int:month>", views.monthly_challenge_by_number),
   path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
