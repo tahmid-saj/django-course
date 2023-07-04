fc260b5fabd50b56295f8752fe0abff9bf9b5895
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-03 20:26:13 -0400

    Returning html

--- section-3-urls-views/monthly_challenges/challenges/views.py
+++ section-3-urls-views/monthly_challenges/challenges/views.py
@@ -44,7 +44,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
+    response_data = f"<h1>{ challenge_text }</h1>"
   except:
-    return HttpResponseNotFound("This month is not suppoerted")
+    return HttpResponseNotFound("<h1>This month is not suppoerted</h1>")
 
-  return HttpResponse(challenge_text)
+  return HttpResponse(response_data)
