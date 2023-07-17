73341806c9db1f683abe86d7d8cf45747471c861
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-16 22:19:43 -0400

    Partial templates

--- section-4-templates-static-files/monthly_challenges/challenges/urls.py
+++ section-4-templates-static-files/monthly_challenges/challenges/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   # path("january", views.january),
   # path("february", views.feburary),
   # path("march", views.march),
-  path("", views.index),
+  path("", views.index, name="index"),
   path("<int:month>", views.monthly_challenge_by_number),
   path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
