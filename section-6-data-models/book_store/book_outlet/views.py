53fc0342db35588c99c00ac53fea2828852dc9a3
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-23 17:13:13 -0400

    Rendering details

--- section-6-data-models/book_store/book_outlet/views.py
+++ section-6-data-models/book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -8,4 +9,19 @@ def index(request):
   books = Book.objects.all()
   return render(request, "book_outlet/index.html", {
     "books": books
+  })
+
+def book_detail(request, id):
+  try:
+    book = Book.objects.get(pk=id)
+  except:
+    raise Http404()
+
+  # book = get_object_or_404(Book, id)
+
+  return render(request, "book_outlet/book_detail.html", {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_best_seller": book.is_best_selling
   })
