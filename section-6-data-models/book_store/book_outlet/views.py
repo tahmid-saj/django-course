ce0c9251cf2c34a1aca3cbf16cca5362960919d7
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-23 18:04:49 -0400

    Adding aggregates

--- section-6-data-models/book_store/book_outlet/views.py
+++ section-6-data-models/book_store/book_outlet/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg, Max, Min
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
-  books = Book.objects.all()
+  books = Book.objects.all().order_by("-title") # DESC order
+  num_books = books.count()
+  avg_rating = books.aggregate(Avg("rating"))
+
   return render(request, "book_outlet/index.html", {
-    "books": books
+    "books": books,
+    "total_number_of_books": num_books,
+    "average_rating": avg_rating
   })
 
 def book_detail(request, slug):
