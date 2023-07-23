3fbd4ed2862e253e9b294659c9582a7d40f3723b
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-23 15:14:25 -0400

    Updating models and migrations

--- section-6-data-models/book_store/book_outlet/models.py
+++ section-6-data-models/book_store/book_outlet/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=50)
-    rating = models.IntegerField()
-    
+  title = models.CharField(max_length=50)
+  rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+  author = models.CharField(null=True, max_length=100)
+  is_best_selling = models.BooleanField(default=False)
+
+
+  def __str__(self):
+    return f"{self.title} ({self.rating})"
