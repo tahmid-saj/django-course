ba8bd342cb4263d9ffa244028a1e7c5fdbecea86
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-23 17:55:31 -0400

    Slugifying books

--- section-6-data-models/book_store/book_outlet/views.py
+++ section-6-data-models/book_store/book_outlet/views.py
@@ -11,13 +11,13 @@ def index(request):
     "books": books
   })
 
-def book_detail(request, id):
-  try:
-    book = Book.objects.get(pk=id)
-  except:
-    raise Http404()
+def book_detail(request, slug):
+  # try:
+  #   book = Book.objects.get(pk=id)
+  # except:
+  #   raise Http404()
 
-  # book = get_object_or_404(Book, id)
+  book = get_object_or_404(Book, slug=slug)
 
   return render(request, "book_outlet/book_detail.html", {
     "title": book.title,
