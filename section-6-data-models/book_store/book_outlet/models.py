0a869e9f305e4cb81b1728d40ffc80f814fedce0
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-23 17:29:53 -0400

    Model urls

--- section-6-data-models/book_store/book_outlet/models.py
+++ section-6-data-models/book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,6 +10,9 @@ class Book(models.Model):
   author = models.CharField(null=True, max_length=100)
   is_best_selling = models.BooleanField(default=False)
 
+  def get_absolute_url(self):
+    return reverse("book-detail", args=[self.id])
+  
 
   def __str__(self):
     return f"{self.title} ({self.rating})"
