dfd5ecd7712332457dd38f94eea0b07eb181ed7c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-31 19:07:40 -0400

    Making migration for author table

--- section-7-admin/book_outlet/models.py
+++ section-7-admin/book_outlet/models.py
@@ -6,12 +6,19 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    # author = models.CharField(null=True, max_length=100)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True) # Could have null if value is not provided
+
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True)
