8829e3f5ea8cfbcb233678f3ea2131fb7390c6e1
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-31 21:00:06 -0400

    Adding many-many using admin

--- section-7-admin/book_outlet/models.py
+++ section-7-admin/book_outlet/models.py
@@ -10,6 +10,12 @@ class Country(models.Model):
     name = models.CharField(max_length=80)
     code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 
 class Address(models.Model):
     street = models.CharField(max_length=80)
