c7c8afd5f8d06bd7b9135c9d916c669ee21492fd
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-07 21:28:49 -0400

    Updating model form

--- section-10-forms/feedback/reviews/forms.py
+++ section-10-forms/feedback/reviews/forms.py
@@ -15,4 +15,15 @@ class ReviewForm(forms.ModelForm):
   class Meta:
     model = Review
     fields = "__all__"
+    labels = {
+      "user_name": "Your Name",
+      "review_text": "Your Feedback",
+      "rating": "Your Rating"
+    }
 
+    error_messages = {
+      "user_name": {
+        "required": "Your name must not be empty",
+        "max_length": "Please enter a shorter name"
+      }
+    }
