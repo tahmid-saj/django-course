cbc50780d24ca49435cd6cd5de31b31cb061bc8f
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-07 23:49:26 -0400

    Form view

--- section-10-forms/feedback/reviews/views.py
+++ section-10-forms/feedback/reviews/views.py
@@ -1,23 +1,33 @@
 from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-  def get(self, request):
-    form = ReviewForm()
+class ReviewView(FormView):
+  form_class = ReviewForm
+  template_name = "reviews/review.html"
+  success_url = "/thank-you"
 
-    return render(request, "reviews/review.html", {
-      "form": form
-    })
+  def form_valid(self, form: Any):
+    form.save()
+    
+    return super().form_valid(form)
+
+  # def get(self, request):
+  #   form = ReviewForm()
+
+  #   return render(request, "reviews/review.html", {
+  #     "form": form
+  #   })
 
   def post(self, request):
     form = ReviewForm(request.POST)
