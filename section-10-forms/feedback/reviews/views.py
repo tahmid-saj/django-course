d34db1c8bd000d3e043e3c886a38a2a546639b75
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-07 22:50:59 -0400

    Template view

--- section-10-forms/feedback/reviews/views.py
+++ section-10-forms/feedback/reviews/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -46,10 +48,12 @@ class ReviewView(View):
 #     "form": form
 #   })
 
+class ThankYouView(TemplateView):
+  template_name = "reviews/thank_you.html"
 
-def thank_you(request):
-  return render(request, "reviews/thank_you.html", {
-    "has_error": False
-  })
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["message"] = "This works"
 
+    return context
 
